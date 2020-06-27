ecb1a38d13456ce4d85a3087a36448dc846a81e6
mli <mli@student.42.fr>
2020-06-27 17:35:50 +0200

    ycm ./

--- .config/.ycm_extra_conf.py
+++ .config/.ycm_extra_conf.py
@@ -1,6 +1,6 @@
 def Settings( **kwargs ):
     return {
-        'flags': ['-x', 'c', '-Wall', '-Wextra', '-Werror', '-I../',
+        'flags': ['-x', 'c', '-Wall', '-Wextra', '-Werror', '-I./', '-I../',
                   '-I./libft', '-I../libft',
                   '-I./libft/includes', '-I../libft/includes',
                   '-I./includes', '-I../includes'
