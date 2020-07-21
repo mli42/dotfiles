6f56ae146367dd23c682ee62e1bcea34dba49532
mli <mli@student.42.fr>
2020-07-22 01:10:32 +0200

    YCM c++

--- .config/.ycm_extra_conf.py
+++ .config/.ycm_extra_conf.py
@@ -5,4 +5,9 @@ def Settings( **kwargs ):
                   '-I./libft/includes', '-I../libft/includes',
                   '-I./includes', '-I../includes'
                   ],
+        'flags': ['-x', 'c++', '-Wall', '-Wextra', '-Werror', '-I./', '-I../',
+                  '-I./libft', '-I../libft',
+                  '-I./libft/includes', '-I../libft/includes',
+                  '-I./includes', '-I../includes'
+                  ],
     }
