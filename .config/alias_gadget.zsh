b979ea0158549a65d10f7e03bb58329dee203868
mli42 <mli@student.42.fr>
2020-10-19 12:21:46 +0200

    gogo_* use associative array

--- .config/alias_gadget.zsh
+++ .config/alias_gadget.zsh
@@ -1,25 +1,33 @@
 projectsdir="~/Desktop/Projects/"
-
 alias gogo_desk="cd ~/Desktop"
-alias gogo_projects="cd $projectsdir"
 
-alias gogo_fac="cd $projectsdir/Python_Fac"
-alias gogo_working_on="cd $projectsdir/working_on"
+# Creating associative array
+declare -A gogo
+
+gogo[projects]="";
+gogo[fac]="Python_Fac"
+gogo[working_on]="working_on"
+gogo[libft]="libft"
+gogo[gnl]="GNL"
+gogo[printf]="ft_printf"
+gogo[cub3D]="cub3D"
+gogo[bootcamp_py]="bootcamp_py"
+gogo[server]="ft_server"
+gogo[exams]="exams/exam04"
+gogo[asm]="libasm"
+gogo[minishell]="charmstr"
+gogo[services]="ft_services"
+gogo[cpp]="cpp-piscine"
+gogo[philo]="philosophers"
+
+for key in ${(k)gogo}; do
+	eval "alias gogo_$key=\"cd $projectsdir/$gogo[$key]\""
+done
+
+unset gogo
+
 alias thp="cd $projectsdir/THP"
-alias gogo_libft="cd $projectsdir/libft"
-alias gogo_gnl="cd $projectsdir/GNL"
-alias gogo_printf="cd $projectsdir/ft_printf"
-alias gogo_cub3D="cd $projectsdir/cub3D"
-alias gogo_bootcamp_py="cd $projectsdir/bootcamp_py"
-alias gogo_server="cd $projectsdir/ft_server"
-alias gogo_exams="cd $projectsdir/exams/"
-alias gogo_asm="cd $projectsdir/libasm"
-alias gogo_minishell="cd $projectsdir/charmstr"
-alias gogo_services="cd $projectsdir/ft_services"
-alias gogo_cpp="cd $projectsdir/cpp-piscine"
-alias gogo_philo="cd $projectsdir/philosophers"
-
 alias doruby="cd $projectsdir/THP_rendu"
 alias dobot="cd $projectsdir/MeandreBot"
 
-# gogo_philo
+# gogo_projects
