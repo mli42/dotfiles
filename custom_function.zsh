e504ddd86c0f6b62a82edd9830697512af33a254
mli42 <mli@student.42.fr>
2020-11-21 16:35:05 +0100

    resetVSC()

--- custom_function.zsh
+++ custom_function.zsh
@@ -96,3 +96,10 @@ function rpath () {
 	done
 	echo "$PATH"
 }
+
+function resetVSC () {
+	rm -rf "$HOME/.vscode"
+	rm -rf "$HOME/Library/Application Support/Code"
+	rm -rf "$HOME/Library/Caches/com.microsoft.VSCode"
+	rm -rf "$HOME/Library/Saved Application State/com.microsoft.VSCode.savedState"
+}
